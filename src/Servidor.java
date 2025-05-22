c67eaa6fbcc8f84620ef2e8d0f39a26f74d01185
QziuL <lf.quinholi2@gmail.com>
2025-05-21 23:47:40 -0300

    feat: logica do servidor para receber e armazenar os clientes que se conectam + tentativa de fazer ele redirecionar as mensagens para outros clientes (nao esta funcionando)

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,16 +1,29 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class Servidor {
+public class Servidor extends Thread {
     public static void main(String[] args) {
-        final int PORTA = 8888;
-        final int PORTA_ESCRITA = 9999;
+        Servidor servidor = new Servidor();
+        servidor.start();
+    }
 
-        try (DatagramSocket socket = new DatagramSocket(PORTA);
-             DatagramSocket socketWrite = new DatagramSocket(PORTA_ESCRITA))
+    public void run() {
+        final int PORTA = 8888;
+        final int BUFFER_SIZE = 1024;
+
+        Set<String> clients = new HashSet<>();
+        List<Integer> ports = new ArrayList<>();
+        List<InetAddress> addresses = new ArrayList<>();
+
+        try (DatagramSocket socket = new DatagramSocket(PORTA))
         {
-            byte[] bufferReceber = new byte[1024];
+            byte[] bufferReceber = new byte[BUFFER_SIZE];
 
             System.out.println("Servidor escutando na porta " + PORTA + "...");
 
@@ -18,21 +31,43 @@ public class Servidor {
                 DatagramPacket pacoteRecebido = new DatagramPacket(bufferReceber, bufferReceber.length);
                 socket.receive(pacoteRecebido);
 
+                String id = pacoteRecebido.getAddress().getHostAddress() + ',' + pacoteRecebido.getPort();
+                if (!clients.contains(id)) {
+                    clients.add(id);
+                    ports.add(pacoteRecebido.getPort());
+                    addresses.add(pacoteRecebido.getAddress());
+                }
+
                 String mensagem = new String(pacoteRecebido.getData(), 0, pacoteRecebido.getLength());
                 System.out.println("Mensagem recebida: [" + mensagem + "]");
 
-                // Enviar resposta
-                String resposta = "Servidor recebeu: " + mensagem;
-                byte[] bufferResposta = resposta.getBytes();
-
-                DatagramPacket pacoteResposta = new DatagramPacket(
-                        bufferResposta,
-                        bufferResposta.length,
-                        pacoteRecebido.getAddress(),
-                        pacoteRecebido.getPort()
-                );
-
-                socketWrite.send(pacoteResposta);
+                if(addresses.size() < 2) {
+                    DatagramPacket pacoteResposta;
+                    byte[] data = ("Você está sozinho...").getBytes();
+                    pacoteResposta = new DatagramPacket(
+                            data,
+                            data.length,
+                            addresses.getFirst(),
+                            ports.getFirst()
+                    );
+                    socket.send(pacoteResposta);
+                } else {
+                    DatagramPacket pacoteResposta;
+                    byte[] data = (id + " : " +  mensagem).getBytes();
+                    for(int i = 0; i < addresses.size(); i++) {
+                        if(addresses.get(i) != pacoteRecebido.getAddress()) {
+                            InetAddress address = addresses.get(i);
+                            int port = ports.get(i);
+                            pacoteResposta = new DatagramPacket(
+                                    data,
+                                    data.length,
+                                    address,
+                                    port
+                            );
+                            socket.send(pacoteResposta);
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
